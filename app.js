9b7f8e460083f5cb6d4ab0ea2795df551159f04a
Simon Carr <simonjcarr@gmail.com>
2019-12-15 22:09:31 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const git = require('simple-git')
+require('dotenv').config()
+
+const app = express()
+const port = 3888
+
+app.post('/api/git', (req, res)=>{
+    const USER = process.env.GIT_USER
+    const PASS = process.env.GIT_PASS
+    const API_REPO_URL = process.env.API_REPO_URL
+    const remote = `https://${USER}:${PASS}@${API_REPO_URL}`
+    git().silent(true)
+    .pull(remote,'master')
+    .then(()=>console.log("Pulled API Repo"))
+    .catch((err)=>console.log("Error pulling API repo", err))
+})
+
+app.listen(port, ()=>console.log(`Express listening on port ${port}`))
